b03e2488e263ffdd38846b828702a2be2c73b668
jordan jones <copypastapublishing@outlook.com>
2018-08-11 19:14:33 -0500

    Database

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: node app.js
+worker: python db_add.py
---
+++ db_add.py
@@ -0,0 +1,35 @@
+import psycopg2
+import urllib.parse as urlparse
+import os
+import json
+a = open("babelli-copypasta.json", mode="r")
+babelli = json.load(a)
+
+
+url = urlparse.urlparse(os.environ['DATABASE_URL'])
+dbname = url.path[1:]
+user = url.username
+password = url.password
+host = url.hostname
+port = url.port
+
+con = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+            )
+
+con.execute("CREATE TABLE fiction (id serial PRIMARY KEY, book_id integer, title text, author text, subjects text);")
+
+book_id_keys=list(babelli.keys())
+for id in book_id_keys:
+    a=babelli[id]["id"]
+    b=babelli[id]["title"]
+    c=babelli[id]["author"]
+    d=" ".join(babelli[id]["subjects"])
+    con.execute("INSERT INTO fiction (book_id, title, author, subjects) VALUES (%a, %b, %c, %d)", )
+
+
+
