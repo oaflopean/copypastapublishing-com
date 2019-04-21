4e7d9a3b962256f062ad5effb8efad02a4c92c9a
Jordan <submissions@copypastapublishing.com>
2019-04-21 07:51:31 -0500

    4/21

--- routes/pitch.js
+++ routes/pitch.js
@@ -2,5 +2,5 @@ exports.pitch = function(req, res){
 res.render('pitch', { title: 'Copypasta Publishing' });
 };
 exports.invitation = function(req, res){
-res.render('images/huge.png');
+res.render('invite', { title: 'Huge Impossible Word Search' });
 };
