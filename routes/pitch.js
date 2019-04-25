4ebdafa06fb841eaff07aacef52626084ef9ff10
oaflopean <oaflopean@gmail.com>
2019-04-25 11:05:03 -0500

    4/25

--- routes/pitch.js
+++ routes/pitch.js
@@ -1,5 +1,5 @@
 exports.pitch = function(req, res){
-res.render('pitch', { title: 'Copypasta Publishing' });
+res.render('pitch', { title: 'Copypasta Publishing: Magenta Madness and Prismatic Displays' });
 };
 exports.invitation = function(req, res){
 res.render('invite', { title: 'Huge Impossible Word Search' });
